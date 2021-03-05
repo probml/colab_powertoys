2b311f2760e93402dd2caeb145cff34c578bdf2b
Mahmoud Soliman <mjsML@users.noreply.github.com>
2021-03-05 18:18:50 +0200

    Update probml_toys.py

--- probml_toys.py
+++ probml_toys.py
@@ -1,15 +1,11 @@
-
+from .toys import toys
 import os
-class probml_toys(object):
+class probml_toys():
     """docstring for probml_toys."""
     def __init__(self):
-        print("Init")
-        os.system('git clone https://github.com/probml/pyprobml &> /dev/null')
-        os.system('pip3 install nbimporter')
-
+        os.environ["PYPROBML"] = '/pyprobml/'
     @staticmethod
     def save_fig(fname,verbose=False, *args, **kwargs):
-        from .toys import toys
         if "PYPROBML" in os.environ:
             root = os.environ["PYPROBML"]
             figdir = os.path.join(root, 'figures')
@@ -19,15 +15,15 @@ class probml_toys(object):
     @staticmethod
     def git_ssh(git_command, email="murphyk@gmail.com", username="probml",
         verbose=False):
-        from .toys import toys
         toys.git_ssh(git_command, email,username,verbose)
     @staticmethod
     def show_image(img_path,size=600):
-      from .toys import toys
       toys.show_image(img_path,size)
     @staticmethod
     def show_and_run(script, i=True):
-      from .toys import toys
+      import inspect
+      #print(toys)
+      #print(inspect.getmembers(toys))
       toys.show_and_run(script,i)
-
+        
 p=probml_toys()
